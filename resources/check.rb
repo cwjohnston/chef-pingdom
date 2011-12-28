1621cf315be7736fdb9b048b7a54cb87fe70fdd4
Cameron Johnston <cameron@needle.com>
2011-12-28 15:47:47 -0700

    modify check provider to use new_resource.type instead of setting the check type locally; rename new_resource.params to new_resource.check_params as it seems 'params' is a reserved parameter; add type parameter to check resource definition.

--- resources/check.rb
+++ resources/check.rb
@@ -6,6 +6,7 @@ end
 actions :add, :delete
 
 attribute :name, :kind_of => String, :name_attribute => true
+attribute :type, :kind_of => String, :required => true, :default => 'http', :regex => /http|tcp|udp|ping|dns|smtp|pop3|imap/
 attribute :host, :kind_of => String, :required => true
 attribute :check_params, :kind_of => [ NilClass, Hash ]
 attribute :notification_params, :kind_of => [ NilClass, Hash ]
