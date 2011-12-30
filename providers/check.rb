e4fad6a0e8f913ea4dd03109b2af9f349399e83a
Cameron Johnston <cameron@needle.com>
2011-12-30 11:50:40 -0700

    Revert "adding misc library to provide pingdom_credentials_set?, wrapping existing check provider actions with conditionals that use it."
    This reverts commit 3274147c98c94970f683bfd8b338fbc7ee9ec54f.
    
    Conflicts:
    
    	libraries/misc.rb

--- providers/check.rb
+++ providers/check.rb
@@ -14,28 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include Opscode::Pingdom::Misc
 include Opscode::Pingdom::Check
 
 action :add do
-  if pingdom_credentials_set?
-    if check_exists?(new_resource.name, new_resource.type)
-      Chef::Log.debug("Pingdom: #{new_resource.type} check #{new_resource.name} already exists, so I will not attempt to create it again.")
-    else
-      add_check(new_resource.name, new_resource.host, new_resource.type, new_resource.check_params)
-    end
+  if check_exists?(new_resource.name, new_resource.type)
+    Chef::Log.debug("Pingdom: #{new_resource.type} check #{new_resource.name} already exists, so I will not attempt to create it again.")
+  else
+    add_check(new_resource.name, new_resource.host, new_resource.type, new_resource.check_params)
   end
 end
 
 action :delete do
-  if pingdom_credentials_set?
-    unless check_exists?(new_resource.name, new_resource.type)
-      Chef::Log.debug("Pingdom: #{new_resource.type} check #{new_resource.name} does not exist, so I will not attempt to delete it.")
-    else
-      check_id = get_check_id(new_resource.name, new_resource.type)
-      Chef::Log.debug("Pingdom: resolved check #{new_resource.name} of new_resource.type #{new_resource.type} to check id #{check_id}")
-      unless delete_check(check_id)
-      end
+  unless check_exists?(new_resource.name, new_resource.type)
+    Chef::Log.debug("Pingdom: #{new_resource.type} check #{new_resource.name} does not exist, so I will not attempt to delete it.")
+  else
+    check_id = get_check_id(new_resource.name, new_resource.type)
+    Chef::Log.debug("Pingdom: resolved check #{new_resource.name} of new_resource.type #{new_resource.type} to check id #{check_id}")
+    unless delete_check(check_id)
     end
   end
 end
