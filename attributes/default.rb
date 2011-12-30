b253be252e4c1dfc3b23e23ab4e86eaa3944aae8
Cameron Johnston <cameron@needle.com>
2011-12-30 15:12:40 -0700

    renaming default node attributes to match resource attribute names

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default[:pingdom][:api_key] = String.new
-default[:pingdom][:api_user] = String.new
-default[:pingdom][:api_pass] = String.new
+default[:pingdom][:username] = String.new
+default[:pingdom][:password] = String.new
