42b2e0dacce76b9018549c45746f46e4efad6332
Cameron Johnston <cameron@needle.com>
2012-11-05 13:37:14 -0700

    bumping cookbook version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "cameron@rootdown.net"
 license          "Apache 2.0"
 description      "Provides LWRPs for managing contacts and service checks on Pingdom's monitoring service."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.5"
+version          "0.2.6"
